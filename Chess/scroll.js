082454befa63fc3ed90bbfd35106f104f5cafc53
dbkkiwnl <87771348+dbkkiwnl@users.noreply.github.com>
2023-04-23 11:31:21 +0800

    Update scroll.js

--- Chess/scroll.js
+++ Chess/scroll.js
@@ -1,3 +1,4 @@
 const navigationHeight = document.querySelector('.topnav ul').offsetHeight;
-
 console.log(navigationHeight);
+
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
