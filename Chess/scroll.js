fc46e4f738af297904fae26458f0aac668752435
dbkkiwnl <87771348+dbkkiwnl@users.noreply.github.com>
2023-06-22 21:21:26 +0800

    Delete Chess directory

--- Chess/scroll.js
+++
@@ -1,4 +0,0 @@
-const navigationHeight = document.querySelector('.topnav ul').offsetHeight;
-console.log(navigationHeight);
-
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 9 + "px");
